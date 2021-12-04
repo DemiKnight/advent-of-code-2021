a6bd7f497da6218c16e90326c111e821934eb572
Alex Knight <alex@alexknight.co.uk>
2021-12-04 23:20:26 +0000

    Working on part 2 of Day 4

--- day4/src/main/scala/uk/co/alexknight/aoc2021/Day4.scala
+++ day4/src/main/scala/uk/co/alexknight/aoc2021/Day4.scala
@@ -9,8 +9,10 @@ object Day4 extends App {
   type BingoCard = Seq[Seq[Int]]
   case class WinningCard(card: Option[BingoCard] = None, winningNumbs: Seq[Int] = Seq.empty)
 
+  case class BingoCardFinder(outstandingCards: Seq[BingoCard], foundCards: Seq[BingoCard] = Nil, winningNums: Seq[Int] = Nil)
+
   def hasWon(board: BingoCard, winningNumbers: Seq[Int]): Boolean = {
-    val rowComplete: Boolean = board.contains { row: Seq[Int] =>
+    val rowComplete: Boolean = board.exists { row: Seq[Int] =>
       row.diff(winningNumbers).isEmpty
     }
 
@@ -23,6 +25,12 @@ object Day4 extends App {
 
     rowComplete || columnComplete
   }
+
+  def getScore(board: BingoCard, numbers: Seq[Int]): Int = {
+    val flatBoardSum = board.foldLeft(Seq.empty[Int])((acc,value) => acc ++ value).diff(numbers).sum
+    numbers.last * flatBoardSum
+  }
+
   // Load data
   val arrayDepth = 5
   val file: Seq[String] = Using(Source.fromResource("input.txt"))(_.getLines.toSeq).getOrElse(Nil)
@@ -36,6 +44,21 @@ object Day4 extends App {
         .toSeq
     }
   ).toSeq
+
+  val result1Mirror: BingoCardFinder = numberCalls.foldLeft(BingoCardFinder(bingoCards)){ (acc, value) =>
+    if(acc.outstandingCards.isEmpty) {
+      acc
+    } else {
+      val newNumberCalls: Seq[Int] = acc.winningNums :+ value
+
+      val (found: Seq[BingoCard], outstanding: Seq[BingoCard]) = acc.outstandingCards.partition(hasWon(_, newNumberCalls))
+
+      acc.copy(winningNums = newNumberCalls,
+        outstandingCards = outstanding,
+        foundCards = acc.foundCards ++ found
+      )
+    }
+  }
 
   val result1Card: WinningCard = numberCalls.foldLeft(WinningCard()) { (acc, number: Int) =>
     if(acc.card.isDefined) {
@@ -54,8 +77,8 @@ object Day4 extends App {
     result1Card.winningNumbs.last * sumOfUnmarked
   }
 
-  println(s"Part1: $result1 / $result1Card")
+  println(s"Part1: $result1 ${getScore(result1Card.card.get, result1Card.winningNumbs)}  ${result1Mirror.foundCards.foreach(x => println(x.head.head))}")
 
   val result2 = 2
-  println(s"Part2: ")
+  println(s"Part2:")
 }
